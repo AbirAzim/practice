2ebfa960f8e2b68f19d9517cd92ab91c95ffe5e9
AbirAzim <azim35-1713@diu.edu.bd>
2026-08-20 00:08:30 +0600

    Add type definitions and example function in prac.ts

---
+++ prac.ts
@@ -0,0 +1,13 @@
+type dateProps = keyof Date & string;
+
+let a = { a: { b: [2, 3, 'foo', null, false] }, 1: false };
+
+type b = typeof a;
+
+type c = keyof typeof a; // is equ c = 'a' | 1
+
+let d: c = 1;
+
+const fun = (k: c) => console.log(k);
+
+fun('a');
